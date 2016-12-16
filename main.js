10ce85c4fe9c74e2f394c989e0b3ae389ca7782c
Billy Hardy <hardywb@leidos.com>
2016-12-15 21:39:19 -0500

    added main.js that forwards client messages to all other clients

---
+++ main.js
@@ -0,0 +1,15 @@
+let Rx = require('rx-js');
+var ws = require('ws').Server,
+    wss = new WebSocketServer({ port: process.env.PORT});
+console.log("Deploy port: " + process.env.PORT);
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        wss.clients.forEach(function(client) {
+            if(ws != client) {
+                client.send(message);
+            }
+        });
+    });
+});
