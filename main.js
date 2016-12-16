e77af81453fafbb715b9def3ed4115ca40363fe8
Billy Hardy <hardywb@leidos.com>
2016-12-16 18:45:51 -0500

    add parsing of message and peel off recipient property

--- main.js
+++ main.js
@@ -6,10 +6,15 @@ console.log("Deploy port: " + process.env.PORT);
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
+        let json = JSON.parse(message);
+        if(json.recipient) {
+         // todo: send to recipient
+        } else {
         wss.clients.forEach(function(client) {
             if(ws != client) {
                 client.send(message);
             }
         });
+        }
     });
 });
