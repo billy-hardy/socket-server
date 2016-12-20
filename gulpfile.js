925e079ee3eb24fab8faa1de956bb4d18a8a08a7
Billy Hardy <hardywb@leidos.com>
2016-12-20 17:23:12 -0500

    fix build process for heroku. Serve files from dist for client

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
 var runSequence = require('run-sequence');
+
 
 gulp.task('default', function () {
     return runSequence('server', 'client');
@@ -11,13 +13,19 @@ gulp.task('server', function() {
 });
 
 gulp.task('client', function () {
-    return runSequence('browserify');
+    return runSequence('browserify', 'move');
 });
 
 gulp.task('browserify', function() {
     return gulp.src('./client/*.js')
-    .pipe(browserify({
-        insertGlobals: true
-    }))
-    .pipe(gulp.dest('./client/build/js'));
+        .pipe(browserify({
+            insertGlobals: true
+        }))
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('move', function () {
+    return gulp.src(['./client/index.html', './client/favicon.ico'])
+        .pipe(gulp.dest('./dist'));
 });
