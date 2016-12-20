e8e5bbb1c208c0052da81c4c75873d3063af87e6
Billy Hardy <hardywb@leidos.com>
2016-12-19 21:18:55 -0500

    add http server that serves index.html

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var Server = require('socket.io');
 
-//var io = new Server(process.env.PORT);
-//console.log("Deploy port: " + process.env.PORT);
-var io = new Server(8080);
-console.log("Deploy port: " + 8080);
+var app = require('http').createServer(handler);
+var io = new Server(app);
+var fs = require('fs');
+var port = 8080;
+//var port = process.env.PORT;
+
+app.listen(port);
+console.log("Deploy port: " + port);
 
 io.on('connection', function (socket) {
     socket.on('publish', function(data) {
@@ -11,3 +15,15 @@ io.on('connection', function (socket) {
         socket.broadcast.emit(data);
     });
 });
+
+function handler(req, res) {
+   fs.readFile(__dirname + '/index.html', 
+       function(err, data) {
+           if(err) {
+               res.writeHead(500);
+               return res.end('Error loading index.html');
+           }
+           res.writeHead(200);
+           res.end(data);
+       });
+}
