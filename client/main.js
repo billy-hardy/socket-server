680fbc90e7b1cdcdeeb32e15b74ea7b73c7395df
Billy Hardy <hardywb@leidos.com>
2016-12-21 15:20:53 -0500

    add input that sends message on button click

--- client/main.js
+++ client/main.js
@@ -5,18 +5,18 @@ var socket = io(window.location.origin);
 setInterval(function () {
     socket.emit("pulse", socket.id);
 }, 30000);
-setInterval(function () {
-    socket.send("hello");
-}, 1000);
 
 Handlebars.registerPartial('chatMessage',
     '<li>{{this}}</li>');
 
+window.publishMessage = function publishMessage(message) {
+    socket.send(message);
+};
 
 socket.on("message", function(data) {
     var threadSource = document.getElementById("chatThread").innerHTML;
     var threadTemplate = Handlebars.compile(threadSource);
     console.info(data);
     var html = threadTemplate({messages: [data]});
-    document.body.innerHTML += html;
+    document.getElementById("messages").innerHTML += html;
 });
