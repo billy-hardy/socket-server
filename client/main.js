cbcdb010650309f3000c3addfbf1deab877a4d2f
Billy Hardy <hardywb@leidos.com>
2016-12-21 13:47:59 -0500

    add handlebars and displaying messages to screen

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 var io = require('socket.io-client');
+var Handlebars = require("handlebars");
 
 var socket = io(window.location.origin);
 setInterval(function () {
@@ -8,6 +9,14 @@ setInterval(function () {
     socket.send("hello");
 }, 1000);
 
+Handlebars.registerPartial('chatMessage',
+    '<li>{{this}}</li>');
+
+
 socket.on("message", function(data) {
+    var threadSource = document.getElementById("chatThread").innerHTML;
+    var threadTemplate = Handlebars.compile(threadSource);
     console.info(data);
+    var html = threadTemplate({messages: [data]});
+    document.body.innerHTML += html;
 });
