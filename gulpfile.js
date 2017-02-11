3929d569575ede99c7dcff61e67f328fffd7b2ea
Billy Hardy <hardywb@leidos.com>
2017-02-11 18:13:37 -0500

    reorder gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,25 @@ gulp.task('default', ['server', 'client']);
 gulp.task('server', ['move:server']); 
 gulp.task('client', ['move:client']);
 
+gulp.task('move:client', ['browserify'], function () {
+    return gulp.src(['./client/index.html', './client/favicon.ico'])
+        .pipe(gulp.dest(CLIENT_DEPLOY));
+});
+
+gulp.task('browserify', ['jsLint:client'], function() {
+    return gulp.src('./client/*.js')
+        .pipe(browserify({
+            insertGlobals: true
+        }))
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest(CLIENT_DEPLOY));
+});
+
+gulp.task('move:server', ['jsLint:server'], function () {
+    return gulp.src(['./server/main.js'])
+        .pipe(gulp.dest(SERVER_DEPLOY));
+});
+
 gulp.task('jsLint:server', jsLint.bind(this, paths.server));
 gulp.task('jsLint:client', jsLint.bind(this, paths.client));
 
@@ -29,21 +48,3 @@ function jsLint(filePath) {
         .pipe(jshint.reporter('fail'));
 }
 
-gulp.task('browserify', ['jsLint:client'], function() {
-    return gulp.src('./client/*.js')
-        .pipe(browserify({
-            insertGlobals: true
-        }))
-        .pipe(concat('build.js'))
-        .pipe(gulp.dest(CLIENT_DEPLOY));
-});
-
-gulp.task('move:client', ['browserify'], function () {
-    return gulp.src(['./client/index.html', './client/favicon.ico'])
-        .pipe(gulp.dest(CLIENT_DEPLOY));
-});
-
-gulp.task('move:server', ['jsLint:server'], function () {
-    return gulp.src(['./server/main.js'])
-        .pipe(gulp.dest(SERVER_DEPLOY));
-});
