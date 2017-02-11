d518a6228a9d8dd58dd10bc13df3bf7bd2375455
Billy Hardy <hardywb@leidos.com>
2017-02-11 16:32:20 -0500

    restructured tasks to have dependencies instead of runSequence

--- gulpfile.js
+++ gulpfile.js
@@ -5,44 +5,42 @@ var SERVER_DEPLOY = DEPLOY_DIR;
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
-var runSequence = require('run-sequence');
 var jshint = require('gulp-jshint');
 var notify = require('gulp-notify');
 
+var paths = {
+    server: 'server/**/*.js',
+    client: 'client/**/*.js'
+};
 
-gulp.task('default', function () {
-    return runSequence('jsLint', 'server', 'client');
-});
+gulp.task('default', ['server', 'client']);
+gulp.task('server', ['move:server']); 
+gulp.task('client', ['move:client']);
 
-gulp.task('jsLint', function () {
-    return gulp.src(['server/**/*.js', 'client/**/*.js'])
-    .pipe(jshint({
-        eqeqeq: true,
-        esversion: 6
-    }))
-    .pipe(notify(function (file) {
-        if(file.jshint.success) {
-            //Don't show something if success
-            return false;
-        }
-        var errors = file.jshint.results.map(function (data) {
-            if(data.error) {
-                return '(' +data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+gulp.task('jsLint:server', jsLint.bind(this, paths.server));
+gulp.task('jsLint:client', jsLint.bind(this, paths.client));
+
+function jsLint(filePath) {
+    return gulp.src([filePath])
+        .pipe(jshint({
+            eqeqeq: true,
+            esversion: 6
+        }))
+        .pipe(notify(function (file) {
+            if(file.jshint.success) {
+                //Don't show something if success
+                return false;
             }
-        });
-        return file.relative + ' (' + file.jshint.results.length + ' errors)\n' + errors;
-    }));
-});
+            var errors = file.jshint.results.map(function (data) {
+                if(data.error) {
+                    return '(' +data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+                }
+            });
+            return file.relative + ' (' + file.jshint.results.length + ' errors)\n' + errors;
+        }));
+}
 
-gulp.task('server', function() {
-    return runSequence('move:server');
-});
-
-gulp.task('client', function () {
-    return runSequence('browserify', 'move:client');
-});
-
-gulp.task('browserify', function() {
+gulp.task('browserify', ['jsLint:client'], function() {
     return gulp.src('./client/*.js')
         .pipe(browserify({
             insertGlobals: true
@@ -51,12 +49,12 @@ gulp.task('browserify', function() {
         .pipe(gulp.dest(CLIENT_DEPLOY));
 });
 
-gulp.task('move:client', function () {
+gulp.task('move:client', ['browserify'], function () {
     return gulp.src(['./client/index.html', './client/favicon.ico'])
         .pipe(gulp.dest(CLIENT_DEPLOY));
 });
 
-gulp.task('move:server', function () {
+gulp.task('move:server', ['jsLint:server'], function () {
     return gulp.src(['./server/main.js'])
         .pipe(gulp.dest(SERVER_DEPLOY));
 });
