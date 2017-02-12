a9aed3c2d17610f6483f72206b1b77390b8fb8b4
Billy Hardy <hardywb@leidos.com>
2017-02-11 20:21:10 -0500

    use express server

--- server/main.js
+++ server/main.js
@@ -1,12 +1,13 @@
-var Server = require('socket.io');
-
-var app = require('http').createServer(handler);
-var io = new Server(app);
+var express = require('express');
+var app = express();
 var fs = require('fs');
 var port = process.env.PORT || 3000;
+var Server = require('socket.io');
+var io = new Server(app);
 
-app.listen(port);
-console.log("Deploying on port " + port);
+app.listen(port, function() {
+    console.log("Deploying on port " + port);
+});
 
 var socketIdUserMap = {}; 
 
@@ -32,21 +33,22 @@ var chat = io
         });
     });
 
-function handler(req, res) {
-    var path = '/client' + req.url;
-    if(path.charAt(path.length-1) === '/') {
-        path += 'index.html';
-    } 
-    var filename = __dirname + path;
-    fs.readFile(filename, 
-        function(err, data) {
-            if(err) {
-                console.error('Error loading ' + path + ': ' + err);
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
-            console.log('Serving: ' + path);
-            res.writeHead(200);
-            res.end(data);
-        });
-}
+app.route('/')
+    .get(function(req, res) {
+        var path = '/client' + req.url;
+        if(path.charAt(path.length-1) === '/') {
+            path += 'index.html';
+        } 
+        var filename = __dirname + path;
+        fs.readFile(filename, 
+            function(err, data) {
+                if(err) {
+                    console.error('Error loading ' + path + ': ' + err);
+                    res.writeHead(500);
+                    return res.end('Error loading index.html');
+                }
+                console.log('Serving: ' + path);
+                res.writeHead(200);
+                res.end(data);
+            });
+    });
