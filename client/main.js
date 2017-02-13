862bc514d9ee40dd144844c6bd2857122ad33c92
Billy Hardy <hardywb@leidos.com>
2017-02-13 00:28:15 -0500

    users can now be authenticated locally or with the server

--- client/main.js
+++ client/main.js
@@ -6,6 +6,7 @@ var IndexedDBService = require("../services/indexedDBService.js");
 var UserService = require("../services/userService.js");
 var MessageService = require("../services/messageService.js");
 var RestService = require("../services/restService.js");
+var md5 = require("blueimp-md5");
 
 var User = require("../beans/user.js");
 var Message = require("../beans/message.js");
@@ -97,23 +98,35 @@ class IndexController {
     }
 
     _initDBs() {
-        this.keypath = "id";
-
-        this.dbPromise = idb.open('dc2f', 2, upgradeDB => {
-            upgradeDB.createObjectStore(this.userStore, {keyPath: this.keypath});
-            upgradeDB.createObjectStore(this.messageStore, {keyPath: this.keypath});
-        });
-
-        this.userStore = "user";
-        this._userServiceInternal = new IndexedDBService(this.userStore, this.dbPromise, this.keypath);
-        this.userService = new UserService(this._userServiceInternal);
-
         if(window.location.search.includes("local")) {
+            this.keypath = "id";
+            this.dbPromise = idb.open('dc2f', 2, upgradeDB => {
+                upgradeDB.createObjectStore(this.userStore, {keyPath: this.keypath});
+                upgradeDB.createObjectStore(this.messageStore, {keyPath: this.keypath});
+            });
+
+            this.userStore = "user";
+            this._userServiceInternal = new IndexedDBService(this.userStore, this.dbPromise, this.keypath);
+            this.userService = new UserService(this._userServiceInternal);
+
             this.messageStore = "message";
             this._messageServiceInternal = new IndexedDBService(this.messageStore, this.dbPromise, this.keypath);
             this.messageService = new MessageService(this._messageServiceInternal);
         }
         else {
+            let userRestService = function(baseUrl) {
+                this.baseUrl = baseUrl;
+            };
+            userRestService.prototype = Object.create(RestService.prototype);
+            userRestService.prototype.authenticate = function(username, password) {
+                return this._constructRequest(this.baseUrl+'/auth/'+username+'/'+md5(password), 'post')
+                    .then(users=> users[0]);
+            };
+            userRestService.prototype.getByUsername = function(username) {
+                return this._constructRequest(this.baseUrl+'/by-username/'+username, 'get')
+                    .then(users=> users[0]);
+            };
+            this.userService = new UserService(new userRestService(window.location.origin+'/users'));
             this.messageService = new MessageService(new RestService(window.location.origin+"/messages"));
         }
         window.messageService = this.messageService;
